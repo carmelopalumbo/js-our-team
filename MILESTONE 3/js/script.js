f6d71561ab104f26c35a8495e5c0ec003271c830
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-25 15:46:18 +0200

    add-bonus

--- MILESTONE 3/js/script.js
+++ MILESTONE 3/js/script.js
@@ -7,37 +7,43 @@ const team = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg'
+        image: 'wayne-barnett-founder-ceo.jpg',
+        adjectives: 'Our leader. Determination and ambition to achieve the goal.'
     },
 
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg'
+        image: 'angela-caroll-chief-editor.jpg',
+        adjectives: 'The one who transforms good ideas into fantastic projects.'
     },
 
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg'
+        image: 'walter-gordon-office-manager.jpg',
+        adjectives: 'Everything we need for the management of our offices.'
     },
 
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg'
+        image: 'angela-lopez-social-media-manager.jpg',
+        adjectives: 'The one who makes our social pages fantastic. Follow us!'
     },
 
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        image: 'scott-estrada-developer.jpg'
+        image: 'scott-estrada-developer.jpg',
+        adjectives: 'Behind great projects there is a great code. Our developer.'
     },
 
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg'
+        image: 'barbara-ramos-graphic-designer.jpg',
+        adjectives: 'Our projects passes through the hands of our fantastic designer.'
     }
     
 ]
@@ -58,6 +64,7 @@ for(let member of team){
         <div class="card-body">
           <p class="card-text fw-bold fs-5">${member.name}</p>
           <p class="card-text fw-lighter">${member.role}</p>
+          <p class="card-text fw-lighter _adjectives">${member.adjectives}</p>
         </div>
       </div>
 </div>`;
